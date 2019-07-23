341fcb38a5fa5f88fa8f0d1d8aa78275b878d72e
allem <allemang.d@gmail.com>
2019-07-23 17:05:45 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='fuzzywuzzydict',
+    version='1.0.0',
+    packages=['fuzzydict'],
+    url='https://www.github.com/allemangD/fuzzywuzzydict',
+    license='MIT',
+    author='allemangD',
+    description='Fuzzy dict key lookup via the fuzzywuzzy package'
+)
